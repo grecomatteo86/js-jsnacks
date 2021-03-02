70a906e79ea9840c87bb0e2cd60f3c3251c92326
Matteo <grecomatteo86@gmail.com>
2021-03-02 11:20:06 +0100

    blocco-1_commit

---
+++ js-jsnacks-blocco-1/js/script.js
@@ -0,0 +1,14 @@
+// l'utente inserisce il primo numero
+var numero1 = prompt('inserisci il primo numero');
+console.log('l\'utente ha inserito come primo numero il ' + numero1);
+
+// l'utente inserisce il secondo numero
+var numero2 = prompt('inserisci il secondo numero');
+console.log('l\'utente ha inserito come secondo numero il ' + numero2);
+
+// stampo il maggiore
+if (numero1 > numero2) {
+  alert(numero1);
+} else {
+  alert(numero2);
+}
