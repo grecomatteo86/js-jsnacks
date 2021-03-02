04a5b0dd5484c00a06e473f4da3155c4bc758625
Matteo <grecomatteo86@gmail.com>
2021-03-02 11:24:36 +0100

    fixes

--- js-jsnacks-blocco-1/js/script.js
+++ js-jsnacks-blocco-1/js/script.js
@@ -1,14 +1,17 @@
 // l'utente inserisce il primo numero
-var numero1 = prompt('inserisci il primo numero');
+var numero1 = parseInt(prompt('inserisci il primo numero'));
 console.log('l\'utente ha inserito come primo numero il ' + numero1);
 
 // l'utente inserisce il secondo numero
-var numero2 = prompt('inserisci il secondo numero');
+var numero2 = parseInt(prompt('inserisci il secondo numero'));
 console.log('l\'utente ha inserito come secondo numero il ' + numero2);
 
 // stampo il maggiore
 if (numero1 > numero2) {
   alert(numero1);
-} else {
+} else if (numero2 > numero1) {
   alert(numero2);
+}
+else {
+  alert('i numeri sono uguali');
 }
