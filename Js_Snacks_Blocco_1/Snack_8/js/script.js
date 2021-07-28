9708cd70e4bba63176268439d25b62eb41b8df02
Matteo <grecomatteo86@gmail.com>
2021-07-28 11:58:06 +0200

    Block_1_Snack_8: add control and refactoring

--- Js_Snacks_Blocco_1/Snack_8/js/script.js
+++ Js_Snacks_Blocco_1/Snack_8/js/script.js
@@ -1,16 +1,9 @@
-/*
-Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
-*/
-
-var numero = prompt('inserisci un numero di 4 cifre');
-// console.log(numero[0]);
-
-var somma = 0;
-
-for (var i = 0 ; i < numero.length; i++) {
-
-somma = somma + parseInt(numero[i]);
-
+var userNumber = prompt('enter a 4-digit number');
+while(isNaN(userNumber) || userNumber == '' || userNumber.length < 4 || userNumber.length > 4){
+  userNumber = prompt('enter a 4-digit number');
 }
-
-console.log(somma);
+var sum = 0;
+for (var i = 0 ; i < userNumber.length; i++) {
+sum = sum + parseInt(userNumber[i]);
+}
+console.log('the sum of numbers you insert is ' + sum);
