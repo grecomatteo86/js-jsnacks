d37f50e8d15f86f51ed5e8f05567d7359e7f3276
Matteo <grecomatteo86@gmail.com>
2021-07-28 17:22:27 +0200

    Block_2_Snack_1: add controls and refactoring

--- Js_Snacks_Blocco_2/Snack_1/js/script.js
+++ Js_Snacks_Blocco_2/Snack_1/js/script.js
@@ -1,62 +1,25 @@
-/*
-Il software deve chiedere per 5 volte all’utente di inserire un numero.
-Il programma stampa la somma di tutti i numeri inseriti.
-Esegui questo programma in due versioni:
-con il for
-e con il while.
-*/
-
-
-
-/*
-// 1. con il ciclo for
-var somma = 0;
-
-for ( i = 0; i < 5; i++) {
-  var numero = parseInt(prompt('inserisci un numero'));
-  console.log('il numero inserito dall\'utente è ' + numero );
-  somma = somma + parseInt(numero);
-
-}
-
-console.log('la somma dei numeri inseriti dall\'utente è ' + somma );
-*/
-
-
-
-
-/*
-// 2. con il ciclo while
+alert('insert 5 numbers');
+var sum = 0;
 var i = 0;
-var somma = 0;
-
+// 1. using while cycle
 while ( i < 5 ) {
-  var numero = parseInt(prompt('inserisci un numero'));
-  console.log('il numero inserito dall\'utente è ' + numero );
-  somma += parseInt(numero);
+  var userNumberSum = askAndSumNumbers();
   i++;
 }
-
-console.log('la somma dei numeri inseriti dall\'utente è ' + somma );
-*/
-
-
-
-
-
-// 3. con il ciclo do while
-var i = 0;
-var somma = 0;
-
+// 2. using do while cycle
+/*
 do {
-
-  var numero = parseInt(prompt('inserisci un numero'));
-  console.log('il numero inserito dall\'utente è ' + numero );
-  somma += parseInt(numero);
-
+  var userNumberSum = askAndSumNumbers();
   i++;
-
 } while (i < 5);
-
-
-console.log('la somma dei numeri inseriti dall\'utente è ' + somma );
+*/
+console.log('the sum of the user\'s numbers is ' + sum );
+// Function - askAndSumNumbers
+function askAndSumNumbers () {
+  var userNumber = parseInt(prompt('insert a number'));
+  while(isNaN(userNumber)){
+    userNumber = parseInt(prompt('insert a number'));
+  }
+  console.log('the user number is ' + userNumber );
+  return sum += parseInt(userNumber);
+}
