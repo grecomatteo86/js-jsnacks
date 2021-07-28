0bed12e27526dd9956c4e79073a291fadb9b84a8
Matteo <grecomatteo86@gmail.com>
2021-07-28 11:44:04 +0200

    Block_1_Snack_6: add control and refactoring

--- Js_Snacks_Blocco_1/Snack_6/js/script.js
+++ Js_Snacks_Blocco_1/Snack_6/js/script.js
@@ -1,13 +1,8 @@
-/*
-Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente
-*/
-
-
-// 1. chiedo all'utente di inserire un numero
-var numero = prompt('inserisci un numero');
-
-// 2. stampo i cubi dei numeri
-for (var i = 1; i <= numero; i++) {
+var userNumber = prompt('insert a number and I\'ll print the cube');
+while(isNaN(userNumber) || userNumber == ''){
+  userNumber = prompt('insert a NUMBER');
+}
+for (var i = 1; i <= userNumber; i++) {
   Math.pow(i, 3);
   console.log(Math.pow(i, 3));
 }
