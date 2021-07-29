74e114afa9fb3de87c6584588fc4c1bd75389446
Matteo <grecomatteo86@gmail.com>
2021-07-29 14:26:43 +0200

    Block_2_Snack_4: refactoring

--- Js_Snacks_Blocco_2/Snack_4/js/script.js
+++ Js_Snacks_Blocco_2/Snack_4/js/script.js
@@ -1,37 +1,8 @@
-/*
- Crea un array di numeri interi
- e fai la somma di tutti i numeri dispari
-*/
-
-
-var numeri = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-var somma = 0;
-
-/*
-for (i = 0; i < numeri.length; i++) {
-
-  if (numeri[i] % 2 == 1) {
-    somma += numeri[i];
+var numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var sum = 0;
+for ( var i = 0; i < numbersArray.length; i++) {
+  if ( numbersArray[i] % 2 == 1 ) {
+    sum += i;
   }
-
 }
-
-console.log('la somma dei numeri dispari è ' + somma);
-*/
-
-
-
-// Fai la somma di tutti gli elementi che sono in posizione dispari
-
-
-for ( var i = 0; i < numeri.length; i++) {
-
-  if ( i % 2 == 1 ) {
-
-    somma += i;
-
-  }
-
-}
-
-console.log('la somma dei numeri in posizione dispari è ' + somma);
+console.log('the sum of odd numbers is ' + sum);
