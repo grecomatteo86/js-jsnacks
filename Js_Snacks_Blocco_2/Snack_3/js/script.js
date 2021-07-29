ac7f185587eafa30512586f20331e8a2f064af82
Matteo <grecomatteo86@gmail.com>
2021-07-29 11:39:37 +0200

    Block_2_Snack_3: refactoring

--- Js_Snacks_Blocco_2/Snack_3/js/script.js
+++ Js_Snacks_Blocco_2/Snack_3/js/script.js
@@ -1,32 +1,12 @@
-/*
-Generatore di “nomi cognomi” casuali: prendendo una lista
-di nomi e una lista di cognomi, Gatsby vuole generare una
-falsa lista di 3 invitati.
-*/
-
-//1. creo un array di nomi
-var nomi = ['Franco','Ciccio','Pino'];
-console.log('i nomi in elenco sono: ' + nomi);
-
-//2. creo un array di cognomi
-var cognomi = ['Verdi','Bianchi','Rossi'];
-console.log('i cognomi in elenco sono: ' + cognomi);
-
-//3. creo un array vuoto in cui andrò a mettere con le combinazioni di nomi e cognomi
-var nomiCognomiRandom = [];
-
-//4. genero le combinazioni di nomi e cognomi e le inserisco dentro l'array vuoto
+var namesArray = ['Franco','Ciccio','Pino'];
+console.log('the name\'s list is: ' + namesArray);
+var lastnamesArray = ['Verdi','Bianchi','Rossi'];
+console.log('the lastname\'s list is: ' + lastnamesArray);
+var randomArray = [];
 for ( var i = 0; i < 3; i++) {
-
-  var nomeRandom = Math.floor(Math.random() * nomi.length);
-
-  var cognomeRandom = Math.floor(Math.random() * cognomi.length);
-
-  var sommaNomeCognomeRandom = nomi[nomeRandom] + cognomi[cognomeRandom];
-
-  nomiCognomiRandom.push(sommaNomeCognomeRandom);
-
-
+  var randomName = Math.floor(Math.random() * namesArray.length);
+  var randomLastname = Math.floor(Math.random() * lastnamesArray.length);
+  var randomSum = namesArray[randomName] + ' ' + lastnamesArray[randomLastname];
+  randomArray.push(randomSum);
 }
-
-console.log('la lista di nomi e cognomi random è: ' + nomiCognomiRandom);
+console.log('the random names list is: ' + randomArray);
